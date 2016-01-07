62c4a762aac98b9c7619782d7280e3b5dd513bef
Lucas Adam M. Paul <reilithion@gmail.com>
2016-01-07 16:43:18 -0700

    More Thursday work, implemented applySubst, etc.

--- Nanopass.hs
+++ Nanopass.hs
@@ -158,33 +158,69 @@ getNewName srcType dstType = do
 
 -- Generates code for transforming a given type to another type
 generateFun :: Name -> Type -> Type -> M ()
-generateFun name srcType dstType = do
+generateFun name srcType dstType =
   generateFun' name srcType dstType >>= addImplementation
 
-generateFun' :: Name -> Type -> Type -> M Dec
+generateFun' :: Name -> Type -> Type -> M [Dec]
 generateFun' name srcType dstType = do
   -- return TH like the following:
-  --  name = \x -> case x of ...
-  -- calls 'generateClause' for generating clauses of the 'case'
+  --  name x = case x of ...clauses...
+  -- where the clauses are created by 'generateClause'
+
+  -- TODO: handle other kinds of TyConI
+  -- TODO: handle non TyConI
   error "unimplemented: generateFun'"
-  
+
+  (srcName, srcBndrs, srcCons, dstName, dstBndrs, dstCons) <- reifyTypes srcType dstType
+  srcSubst <- unify srcType (foldl AppT (ConT srcName) (map bndrToType srcBndrs))
+
+  TyConI (DataD _ dstName dstBndrs dstCons _) <- reifyHead dstType
+  dstSubst <- unify dstType (foldl AppT (ConT dstName) (map bndrToType dstBndrs))
+
+  arg <- newName "arg"
+  return [SigD name (srcType `AppT` dstType),
+          FunD name [Clause [VarP arg] (NormalB (CaseE (VarE arg) clauses))]]
+
+
+bndrToType :: TyVarBndr -> Type
+bndrToType (PlainTV n) = VarT n
+bndrToType (KindedTV n k) = SigT (VarT n) k
+
+
 type Subst = Map.Map Name Type
 
-applySubst :: Subst -> Type -> Type
-applySubst = error "unimplemented: applySubst"
+applySubst :: Subst -> Type -> Q Type
+applySubst s typ =
+  case typ of
+    ForallT b c t -> do
+      pests <- mapM newName b
+      let pestSubst = Map.fromList $ zip b pests
+          s' = Map.union pestSubst s -- Map.union is left biased
+      ForallT <$> pure pests <*> (mapM (applySubst s') c) <*> (applySubst s' t)
+    AppT l r -> AppT <$> (applySubst s l) <*> (applySubst s r)
+    SigT t k -> SigT <$> (applySubst s t) <*> pure k
+    VarT n ->
+      case Map.lookup n s of
+        Just t -> pure t
+        Nothing -> pure typ
+    _ -> pure typ
 
-generateClause :: Con -> Subst -> Con -> Subst -> M Match
-generateClause srcCon srcSubst dstCon dstSubst = do
+generateClause :: Con -> Subst -> Maybe Con -> Subst -> M Match
+generateClause srcCon srcSubst Nothing dstSubst = do
+  -- TODO: more detailed message
+  body <- lift [|error "translation error"|]
+  Match (RecP srcName []) (NormalB body)
+generateClause srcCon srcSubst (Just dstCon) dstSubst = do
   -- TODO: handle custom user-specified clauses
+  -- TODO: "Maybe Con" if constructor is deleted
 
   -- return TH like the following:
   --  srcCon x1 x2 ... -> dstCon (f1 x1) (f2 x2) ...
   -- calls 'getFun' to find f1, f2, etc.
   let NormalC srcConName srcStrictTypes = srcCon
-  let NormalC dstConName dstStrictTypes = dstCon
   
   let srcArgTypes :: [Type]
-      srcArgTypes = map (applySubst srcSubst. snd) srcStrictTypes
+      srcArgTypes = mapM (applySubst srcSubst . snd) srcStrictTypes
 
   srcArgs :: [Name] <- mapM (\i -> lift $ newName $ "arg"++show i)
                             (zipWith const [1..] srcArgTypes)
@@ -195,7 +231,7 @@ generateClause srcCon srcSubst dstCon dstSubst = do
         return (AppE (VarE funName) (VarE srcArg))
 
   let dstArgTypes :: [Type]
-      dstArgTypes = map (applySubst dstSubst. snd) dstStrictTypes
+      dstArgTypes = mapM (applySubst dstSubst . snd) dstStrictTypes
 
   dstArgs :: [Exp] <- sequence (zipWith3 generateApp srcArgs srcArgTypes dstArgTypes)
 
