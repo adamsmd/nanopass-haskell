3eb53d6de8a6475ad4637fa0d0b3d301fdf484b0
Lucas Adam M. Paul <reilithion@gmail.com>
2016-01-07 14:37:59 -0700

    Thursday work toward an implementation

--- Nanopass.hs
+++ Nanopass.hs
@@ -4,6 +4,7 @@ module Nanopass where
 import Control.Applicative
 import Control.Monad.RWS
 import qualified Data.Map as Map
+import Data.Monoid
 import Language.Haskell.TH
 import Language.Haskell.TH.Syntax hiding (lift)
 
@@ -19,16 +20,20 @@ data Delta where
 type M a = RWST R W S Q a
 
 data R = R {
-  -- Names for functions that the user supplies to us
-  userFunctionsR :: Map.Map (Type{-srcType-}, Type{-dstType-}) Name
+  -- Names for functions that the user supplies to "forward"
+  userFunctionsR :: Map.Map (Type{-srcType-}, Type{-dstType-}) Name,
   
   -- Names for generated functions where the user specifies the name
   namedFunctionsR :: Map.Map (Type{-srcType-}, Type{-dstType-}) Name
   }
   
 data W = W {
-  generatedImplementationsW :: [Exp]
+  generatedImplementationsW :: [Dec]
   }
+
+instance Monoid W where
+  mempty = W mempty
+  mappend (W x) (W y) = W (x <> y)
 
 data S = S {
   worklistS :: [(Name, Type{-srcType-}, Type{-dstType-})],
@@ -42,11 +47,14 @@ popWorklist = do
   case worklistS s of
     [] -> return Nothing
     x:xs -> do put (s { worklistS = xs })
-               return x
+               return (Just x)
 
 addWorklist :: Name -> Type -> Type -> M ()
 addWorklist name srcType dstType =
-  modify (\s -> s { worklistS = name : (worklistS s) })
+  modify (\s -> s { worklistS = (name, srcType, dstType) : (worklistS s) })
+
+addImplementation :: Dec -> M ()
+addImplementation impl = tell (W [impl])
 
 -- returns the names of the functions that
 -- are supplied to 'forward' by the user
@@ -94,13 +102,13 @@ typeDeltas deltas = do
                     generateFun name srcType dstType >> loop
       userFunctions = error "No user functions yet"
       initialState = error "No initial state yet"
-  runRWST loop userFunctions initialState
+  ((), finalState, finalImpls) <- runRWST loop userFunctions initialState
 
   -- return function that looks like:
   --   forward u1 u2 u3 = (e1, e2, e3) where
   --     ... results from calling generateFun ...
   -- 
-
+  error "typeDeltas not implemented"
 
 -- 'getFun' checks if there is already a user supplied
 -- function and if so, does not put anything in the worklist.
@@ -116,44 +124,40 @@ getFun srcType dstType = do
 getFunNoUser :: Type -> Type -> M Name
 getFunNoUser srcType dstType = do
   s <- get
-  case Map.lookup (srcType, dstType) (generatedNamesS s) of
+  let names = generatedNamesS s
+  case Map.lookup (srcType, dstType) names of
     Just name -> return name
     Nothing -> do
-      name <- getName srcType dstType
+      name <- getNewName srcType dstType
+      -- Record the name before starting processing so we properly handle recursion
+      -- Puts (name, error "Impossible in ...") in the generateFunctionsS mapping
       put (s { generatedNamesS = Map.insert (srcType, dstType) name names })
       addWorklist name srcType dstType
       return name
 
 -- looks to see if the user specified a name
 -- otherwise, generates a new name
-getName :: Type -> Type -> M Name
-getName srcType dstType = do
-  s <- get
-  let names = generatedNamesS s
+getNewName :: Type -> Type -> M Name
+getNewName srcType dstType = do
+  names <- asks namedFunctionsR
   case Map.lookup (srcType, dstType) names of
     Just name -> return name
     Nothing -> lift $ newName "go"
 
 
+------------------------
+-- Worklist Functions --
+------------------------
 
 -- Generates code for transforming a given type to another type
 generateFun :: Name -> Type -> Type -> M ()
 generateFun name srcType dstType = do
-  error "generateFun unimplemented" 
-  -- Record the name before starting processing so we properly handle recursion
-  -- Puts (name, error "Impossible in ...") in the generateFunctionsS mapping
-  addGeneratedName srcType dstType name
-  implementation <- error "generateFun not implemented."
-  
-  -- Record the implementation for the generated function
-  addGeneratedImplementation srcType dstType implementation
-  return ()
+  generateFun' name srcType dstType >>= addImplementation
 
 generateFun' :: Name -> Type -> Type -> M Dec
 generateFun' name srcType dstType = do
   -- return TH like the following:
-  --  name :: srcType -> dstType
-  --  name x = case x of ...
+  --  name = \x -> case x of ...
   -- calls 'generateClause' for generating clauses of the 'case'
   error "unimplemented: generateFun'"
 
